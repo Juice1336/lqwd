ff4dd2ea49079fd5d6e3b932255c76da8197c5a8
The Night King <rxainze@live.com>
2021-11-28 14:02:15 +0100

    init

--- server/server.go
+++ server/server.go
@@ -27,6 +27,29 @@ func (s *server) SpawnNodes(ctx context.Context, in *pb.NodeDetails) (*pb.Node, 
 	var user_id string = string(rand.Intn(100))
 	return &pb.Node{NodeName: in.GetNodeName(), Ip: in.GetIp(), Status: (*pb.Node_Status)(in.GetStatus().Enum()), UserId: in.GetUserId(), CreatedAt: in.GetCreatedAt(), Id: user_id}, nil
 }
+func (s *server) GetNodesListByStatus(ctx context.Context, in *pb.Status) (*pb.Node, error) {
+	n := &pb.Node{
+		NodeName:  "aa",
+		Ip:        "121.121.32.12",
+		Status:    pb.Node_RUNNING.Enum(),
+		CreatedAt: "2020/01/02",
+		UserId:    "212121",
+		Id:        "121121",
+	}
+	n1 := &pb.Node{
+		NodeName:  "aWWWaa",
+		Ip:        "121.121.32.12",
+		Status:    pb.Node_FAILED.Enum(),
+		CreatedAt: "2020/01/02",
+		UserId:    "212SWS121",
+		Id:        "sadadada",
+	}
+	if n1.Status == (*pb.Node_Status)(in.Status) {
+		return n1, nil
+	} else {
+		return n, nil
+	}
+}
 
 func main() {
 	lis, err := net.Listen("tcp", port)
